2effe6ae64c390c586fa7f28be609eb0c998bdf0
Stijn Heymans <stijn.heymans@gmail.com>
2020-06-29 08:10:39 -0700

    Add SingleLinkedList delete

--- src/main/net/stijn/java_playground/SingleLinkedList.java
+++ src/main/net/stijn/java_playground/SingleLinkedList.java
@@ -1,5 +1,8 @@
 package net.stijn.java_playground;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.util.Objects;
@@ -14,7 +17,15 @@ public class SingleLinkedList<V> {
      *
      * @param <V> the type of the value of each node
      */
-    private record Node<V>(@NonNull V value, Node<V> next) { }
+    private class Node<V> {
+        private V value;
+        private Node<V> next;
+
+        public Node(@NonNull V value, Node<V> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     /**
      * The root of the linked list.
@@ -46,7 +57,7 @@ public class SingleLinkedList<V> {
         if (Objects.isNull(this.root)) {
             return Optional.empty();
         } else {
-            return Optional.of(this.root.value());
+            return Optional.of(this.root.value);
         }
     }
 
@@ -61,17 +72,42 @@ public class SingleLinkedList<V> {
         int foundIndex = 0;
 
         while (!Objects.isNull(current)) {
-            if (current.value().equals(v)) {
+            if (current.value.equals(v)) {
                 return foundIndex;
             }
-            current = current.next();
+            current = current.next;
             ++foundIndex;
         }
 
         return -1;
     }
 
+    /**
+     * Delete all nodes with a value.
+     *
+     * @param v the value to delete.
+     */
     public void delete(@NonNull V v) {
-        // TODO
+        Node<V> current = this.root;
+        Node<V> previous = null;
+
+        while (!Objects.isNull(current)) {
+            if (current.value.equals(v)) {
+                if (Objects.isNull(previous)) {
+                    // We're actually deleting the root.
+                    this.root = this.root.next;
+                } else {
+                    // Note that this is why we do not store the inner Node as a record,
+                    // we're changing nodes (and a record makes things like `next` final as it's supposed
+                    // to be immutable.
+                    previous.next = current.next;
+                }
+            } else {
+                // Don't move previous if you're deleting current (you do not want previous
+                // to point to a deleted node.)
+                previous = current;
+            }
+            current = current.next;
+        }
     }
 }
--- src/test/net/stijn/java_playground/SingleLinkedListTest.java
+++ src/test/net/stijn/java_playground/SingleLinkedListTest.java
@@ -39,4 +39,26 @@ class SingleLinkedListTest {
         assertEquals(0, list.find("el2"), "el2 is at first place");
         assertEquals(1, list.find("el1"), "el1 is at second place");
     }
+
+    @Test
+    void delete() {
+        SingleLinkedList<String> list = new SingleLinkedList<>();
+        list.add("el1");
+        list.add("el2");
+        list.delete("el2");
+        assertEquals("el1", list.first().orElse("not found"), "el1 is at first place");
+        assertEquals(-1, list.find("el2"), "can't find this it's deleted");
+        assertEquals(0, list.find("el1"), "el1 is there and it's the first");
+    }
+
+    @Test
+    void deleteAll() {
+        SingleLinkedList<String> list = new SingleLinkedList<>();
+        // Add the same element twice.
+        list.add("el2");
+        list.add("el2");
+        list.delete("el2");
+        // The list now has to be empty as we delete all:
+        assertEquals(-1, list.find("el2"));
+    }
 }
